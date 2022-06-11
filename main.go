013c5190eb687e9acd59feea0e6f8ee689b4131c
Magnus <magnus@macbook-pro.local>
2022-06-11 20:17:33 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	_ "embed"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+//go:embed static/index.html
+var index string
+
+func main() {
+
+	r := gin.Default()
+
+	r.GET("/idk", func(c *gin.Context) {
+		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(index))
+	})
+
+	r.Run(":8081")
+
+}
